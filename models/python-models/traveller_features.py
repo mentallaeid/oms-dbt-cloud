4641695ec2eac26b2659a58c9c714601354a657c
mentalla.eid@gmail.com <mentalla.eid@gmail.com>
2026-05-26 14:27:13 +0000

    traveller features

--- models/python-models/traveller_features.py
+++ models/python-models/traveller_features.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from dateutil import parse
+from dateutil import parser
 from sklearn.preprocessing import LabelEncoder
+
 
 def model(dbt, session):
 
     dbt.config(
-        materialized = "table"
+        materialized = "table",
         packages = ["scikit-learn"]
     )
 
@@ -16,17 +17,18 @@ def model(dbt, session):
     def calculate_age(dob_string):
         dob = parser.parse(dob_string)
         age = datetime.today().year - dob.year
-
         return age
 
     df['AGE'] = df['DOB'].apply(calculate_age)
 
     # Encode categorical columns to numeric
-    df['GENDER_ENCODED'] = LabelEncoder.fit_transform(df['GENDER'].astype(str))
-    df['CITY_ENCODED'] = LabelEncoder.fit_transform(df['CITY'].astype(str))
+    le = LabelEncoder()
+    df['GENDER_ENCODED'] = le.fit_transform(df['GENDER'].astype(str))
+    df['CITY_ENCODED'] = le.fit_transform(df['CITY'].astype(str))
 
     # select final columns for output
     df_results = df[['TRAVELLER_ID', 'AGE', 'GENDER_ENCODED', 'CITY_ENCODED']]
 
     return session.create_dataframe(df_results)
+
 
