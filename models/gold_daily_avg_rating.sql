8da210bd6b11495c2577fe58b187afa07e170120
mentalla.eid@gmail.com <mentalla.eid@gmail.com>
2026-05-28 09:48:56 +0000

    added gold layer

---
+++ models/gold_daily_avg_rating.sql
@@ -0,0 +1,7 @@
+SELECT date(date_format(r.created_at, 'yyyy-MM-dd')) AS review_date,
+    r.product_id,
+    p.product_name,
+    AVG(r.rating) AS avg_rating
+FROM {{ ref("bronze_reviews") }} r
+LEFT JOIN {{ ref("silver_products") }} p ON r.product_id = p.id
+GROUP BY all
