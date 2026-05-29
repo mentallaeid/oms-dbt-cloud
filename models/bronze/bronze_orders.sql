7dee9e350ac160f8433cf811b9152140dea5ed4a
mentalla.eid@gmail.com <mentalla.eid@gmail.com>
2026-05-29 15:10:21 +0000

    Added incremental matarialization to orders table

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,4 +1,15 @@
+{{ 
+    config(
+        materialized = 'incremental',
+        unique_key = 'id'
+    ) 
+}}
+
 SELECT *
 FROM {{ source('landing', 'orders') }}
 
 -- dbt_project_catalog.landing.orders
+
+{% if is_incremental() %}
+WHERE created_at > (SELECT COALESCE(MAX(created_at), '1900-01-01') FROM {{ this }})
+{% endif %}
